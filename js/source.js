9ca88900b9bcd052079df7cd0dd51bc87bd09c02
phouse512 <philiphouse2015@u.northwestern.edu>
2014-04-11 11:43:55 -0500

    third demo

--- js/source.js
+++ js/source.js
@@ -65,3 +65,87 @@ function updateButton(sliderPosition){
 	$("#button-image").css("margin-left", sliderPosition*6);
 	$("#submit").fadeIn("slow");
 }
+
+/*
+
+
+	Single Rank 1
+
+
+*/
+
+function switchGrey(){
+	var grey1 = $(".grey1");
+	var grey2 = $(".grey2");
+	var highlighted1 = $(".highlighted1");
+	var highlighted2 = $(".highlighted2");
+	if($(grey1).css("display") == "block"){
+		$(grey1).fadeOut("slow");
+		$(grey2).fadeIn("slow");
+		$(highlighted1).fadeIn("slow");
+		$(highlighted2).fadeOut("slow");
+	} else {
+		$(grey1).fadeIn("slow");
+		$(grey2).fadeOut("slow");
+		$(highlighted1).fadeOut("slow");
+		$(highlighted2).fadeIn("slow");
+	}
+}
+
+function thumbs(object){
+	if($(object).hasClass("selected-icon")){
+		$(object).removeClass("selected-icon");
+	} else {
+		$(object).siblings().removeClass("selected-icon");
+		$(object).addClass("selected-icon");
+	}
+}
+
+function thumbsDown(object){
+	if($(object).hasClass("selected-icon")){
+		$(object).removeClass("selected-icon");
+	} else {
+		$(object).siblings().removeClass("selected-icon");
+		$(object).addClass("selected-icon");
+	}
+}
+
+function nextArea(){
+	$(".selected-icon").removeClass("selected-icon");
+	switchGrey();
+}
+
+///Second ranked example
+
+function switchGrey2(){
+	var grey1 = $(".grey1-part2");
+	var grey2 = $(".grey2-part2");
+	var grey21 = $(".grey2-1-part2");
+	var grey3 = $(".grey3-part2");
+
+	if($(grey1).css("display") == "block"){
+		//move to second part
+		$(grey1).fadeOut("slow");
+		$(grey2).fadeIn("slow");
+		$(grey21).fadeIn("slow");
+	} else if($(grey2).css("display") == "block"){
+		// move to third part
+		$(grey2).fadeOut("slow");
+		$(grey21).fadeOut("slow");
+		$(grey3).fadeIn("slow");
+	} else {
+		// move to first part
+		$(grey3).fadeOut("slow");
+		$(grey1).fadeIn();
+	}
+}
+
+function nextArea2(){
+	$(".selected-icon").removeClass("selected-icon");
+	switchGrey2();
+}
+
+
+
+
+
