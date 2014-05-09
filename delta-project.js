8c870a8ee802f7507823d34aab4ee68177da8cfc
phouse512 <philiphouse2015@u.northwestern.edu>
2014-05-09 05:20:51 -0500

    another push

--- delta-project.js
+++ delta-project.js
@@ -1,51 +1,251 @@
 templateOrder = {
-  'first':'compare-1',
-  'second': 'teaching-module-1',
-  'third': 'teaching-module-2',
-  'fourth': 'vertical_lines',
-  'fifth': 'range_exercise',
-  'sixth': 'choose-1'
+  'first':'compare1',
+  'second': 'compareFeedback',
+  'third': 'teachingModule1',
+  'fourth': 'teachingModule2',
+  'fifth': 'verticalLines',
+  'sixth': 'rangeExercise',
+  'seventh': 'choose-1'
 }
 
-if (Meteor.isClient) {
-  //Template.hello.greeting = function () {
-  //  return "Welcome to delta-project.";
-  //};
+LineDifferentials = new Meteor.Collection("lineDifferentials");
+StrongLines = new Meteor.Collection("strongLines");
+WeakLines = new Meteor.Collection("weakLines");
+WrittenHeuristics = new Meteor.Collection("writtenHeuristics");
 
 
+if(Meteor.isClient){
+
+  // Routing the entire 
   Router.map(function () {
     this.route('first-block', {
       path: '/1', // match the root path
-      template: templateOrder.first
+      template: templateOrder.first,
+      data: {
+        current: 1
+      }
     }),
     this.route('second-block', {
       path: '/2',
-      template: templateOrder.second
+      template: templateOrder.second,
+      data: {
+        current: 2
+      }
     }),
     this.route('third-block', {
       path: '/3',
-      template: templateOrder.third
+      template: templateOrder.third,
+      data: {
+        current: 3
+      }
     }),
     this.route('fourth-block', {
       path: '/4',
-      template: templateOrder.fourth
+      template: templateOrder.fourth,
+      data: {
+        current: 4
+      }
     }),
     this.route('fifth-block', {
       path: '/5',
-      template: templateOrder.fifth
+      template: templateOrder.fifth,
+      data: {
+        current: 5
+      }
     }),
     this.route('sixth-block', {
       path: '/6',
-      template: templateOrder.sixth
+      template: templateOrder.sixth,
+      data: {
+        current: 6
+      }
+    }),
+    this.route('seventh-block', {
+      path: '/7',
+      template: templateOrder.seventh,
+      data: {
+        current: 7
+      }
+    });
+
+    this.route('BeginHeuristics', {
+      path: '/begin-heuristics',
+      template: 'BeginHeuristics'
+    });
+
+    this.route('StrongLinesHeuristics1', {
+      path: '/strong-line-heuristics/1',
+      template: 'StrongLineHeuristic1'
+    });
+
+    this.route('StrongLinesHeuristics2', {
+      path: '/strong-line-heuristics/2',
+      template: 'StrongLineHeuristic2'
+    });
+
+    this.route('WrittenHeuristics1', {
+      path: '/written-heuristics/1',
+      template: 'WrittenHeuristic1'
+    });
+    this.route('WrittenHeuristics2', {
+      path: '/written-heuristics/2',
+      template: 'WrittenHeuristic2'
+    });
+
+    this.route('ViewHeuristics', {
+      path: '/view-heuristics/:_test',
+      template: 'ViewHeuristics',
+      data: function(){
+        console.log(this.params._test);
+        differentials = LineDifferentials.find({image: this.params._test}).fetch();
+        reports = WrittenHeuristics.find({image: this.params._test}, {sort: {date_created: -1}}).fetch();
+        return {differentials: differentials, reports: reports};
+      }
     });
   });
 
-  //
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  /*
+
+    Template event listeners
+
+  */  
+
+  Template.compareFeedback.events({
+    'click #next': function() {
+      newPath = parseInt(Router.current().path.substring(1));
+      newPath = newPath + 1;
+
+      Router.go('/' + newPath);
+    },
+    'mouseover #strongVertical': function() {
+      var c = document.getElementById("target_form_canvas");
+      var ctx = c.getContext("2d");
+      ctx.moveTo(20,0);
+      ctx.lineTo(20, 400);
+      ctx.stroke();
+    }, 
+    'mouseleave #strongVertical': function() {
+      var c = document.getElementById("target_form_canvas");
+      var context = c.getContext('2d');
+      context.beginPath();
+      context.clearRect(0, 0, c.width, c.height);
+    }
+  });
+
+  Template.teachingModule1.events({
+    // move to the next page
+    'click #next': function() {
+      newPath = parseInt(Router.current().path.substring(1));
+      newPath = newPath + 1;
+
+      Router.go('/' + newPath);
+    }
+  });
+  Template.teachingModule2.events({
+    // move to the next page
+    'click #next': function() {
+      newPath = parseInt(Router.current().path.substring(1));
+      newPath = newPath + 1;
+
+      Router.go('/' + newPath);
+    }
+  });
+  Template.verticalLines.events({
+    // move to the next page
+    'click #next': function() {
+      newPath = parseInt(Router.current().path.substring(1));
+      newPath = newPath + 1;
+
+      Router.go('/' + newPath);
+    }
+  });
+  Template.rangeExercise.events({
+    // move to the next page
+    'click #next': function() {
+      newPath = parseInt(Router.current().path.substring(1));
+      newPath = newPath + 1;
+
+      Router.go('/' + newPath);
+    }
+  });
+  Template.compare1.events({
+    // move to the next page
+    'click #next': function() {
+      newPath = parseInt(Router.current().path.substring(1));
+      newPath = newPath + 1;
+
+      Router.go('/' + newPath);
+    }
+  });
+
+  /*Template.BeginHeuristics.events({
+
+  });*/
+
+  Template.StrongLineHeuristic1.events({
+    'click #submit': function() {
+      weakLines = JSON.parse($('#weakArray').val());
+      strongLines = JSON.parse($('#strongArray').val());
+      
+      //differentials = strong - weak
+      userDifferential = strongLines.length - weakLines.length;
+      LineDifferentials.insert({image: 'test1', differential: userDifferential});
+    }
+  });
+
+  Template.StrongLineHeuristic2.events({
+    'click #submit': function() {
+      weakLines = JSON.parse($('#weakArray').val());
+      strongLines = JSON.parse($('#strongArray').val());
+      
+      //differentials = strong - weak
+      userDifferential = strongLines.length - weakLines.length;
+      LineDifferentials.insert({image: 'test2', differential: userDifferential});
+    }
+  });
+
+  // Written Heuristics code
+  Template.WrittenHeuristic1.events({
+    'click #submit': function() {
+      submittedPriority = $("#priority").val();
+      submittedProblem = $("#problem").val();
+      submittedFix = $("#fix").val();
+
+      WrittenHeuristics.insert({image: 'test1', priority: submittedPriority, problem: submittedProblem, fix: submittedFix, date_created: Date.now() });
+      $("#priority").val("");
+      $("#problem").val("");
+      $("#fix").val("");
+      $("#heuristic").fadeOut(function(){
+        $("#werd").fadeIn(function(){
+          setTimeout(function(){
+            $("#werd").fadeOut(function(){
+              $("#heuristic").fadeIn();
+            });
+          }, 1500);
+        });
+      });
+    }
+  });
+
+  Template.WrittenHeuristic2.events({
+    'click #submit': function() {
+      submittedPriority = $("#priority").val();
+      submittedProblem = $("#problem").val();
+      submittedFix = $("#fix").val();
+
+      WrittenHeuristics.insert({image: 'test2', priority: submittedPriority, problem: submittedProblem, fix: submittedFix});
+      $("#priority").val("");
+      $("#problem").val("");
+      $("#fix").val("");
+      $("#heuristic").fadeOut(function(){
+        $("#werd").fadeIn(function(){
+          setTimeout(function(){
+            $("#werd").fadeOut(function(){
+              $("#heuristic").fadeIn();
+            });
+          }, 1500);
+        });
+      });
     }
   });
 }
@@ -55,3 +255,4 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
---
+++ public/js/source.js
@@ -0,0 +1,249 @@
+/*
+
+
+	Choose_one javascript functions
+
+
+*/
+
+linesArray = [];
+
+function selectImage(image){
+	$(".darkClass").removeClass("darkClass");
+	$(".selected-image").removeClass("selected-image");
+
+	$("#" + image).addClass("selected-image");
+	if(image == "image1"){
+		$("#image2").addClass("darkClass");
+	} else {
+		$("#image1").addClass("darkClass");
+	}
+
+	//clear selected options for following questions
+	$(".active").removeClass("active");
+	$("#label-alignment").fadeOut("slow");
+	$("#reasoning").fadeOut("slow");
+	$("#submit").fadeOut("slow");
+
+	//display selection options
+	$("#label-alignment").fadeIn("slow", function(){
+		$("html, body").animate({ scrollTop: $(document).height() }, 1500);
+
+	});
+
+}
+
+function selectImageForFinalChoose(image){
+	$(".darkClass").removeClass("darkClass");
+	$(".selected-image").removeClass("selected-image");
+
+	$("#" + image).addClass("selected-image");
+	if(image == "image1"){
+		$("#image2").addClass("darkClass");
+	} else {
+		$("#image1").addClass("darkClass");
+	}
+
+	//clear selected options for following questions
+	//$(".active").removeClass("active");
+	//$("#label-alignment").fadeOut("slow");
+	//$("#reasoning").fadeOut("slow");
+	$("#submit").fadeIn("slow");
+
+	//display selection options
+	//$("#label-alignment").fadeIn("slow", function(){
+	//	$("html, body").animate({ scrollTop: $(document).height() }, 1500);
+	//
+	//});
+
+}
+
+function selectLabelAlignment(object){
+	$(".label-alignment-selection .active").removeClass("active");
+	$(object).addClass("active");
+
+	$("#reasoning").fadeIn("slow", function(){
+		$("html, body").animate({ scrollTop: $(document).height() }, 1500);
+	});
+}
+
+function selectFactors(object){
+	if($(object).hasClass("active")){
+		$(object).removeClass("active");
+	} else {
+		$(object).addClass("active");
+	}
+
+	$("#submit").show("slow", function(){
+		$("html, body").animate({ scrollTop: $(document).height() }, 1500);
+	});
+}
+
+
+/*
+
+
+	Sliding button
+
+
+*/
+
+function updateButton(sliderPosition){
+	$("#button-image").css("margin-left", sliderPosition*6);
+	$("#submit").fadeIn("slow");
+}
+
+/*
+
+
+	Single Rank 1
+
+
+*/
+
+function switchGrey(){
+	var grey1 = $(".grey1");
+	var grey2 = $(".grey2");
+	var highlighted1 = $(".highlighted1");
+	var highlighted2 = $(".highlighted2");
+	if($(grey1).css("display") == "block"){
+		$(grey1).fadeOut("slow");
+		$(grey2).fadeIn("slow");
+		$(highlighted1).fadeIn("slow");
+		$(highlighted2).fadeOut("slow");
+		$("#goHome").fadeOut("slow");
+	} else {
+		$(grey1).fadeIn("slow");
+		$(grey2).fadeOut("slow");
+		$(highlighted1).fadeOut("slow");
+		$(highlighted2).fadeIn("slow");
+		$("#goHome").fadeIn("slow");
+	}
+}
+
+function thumbs(object){
+	if($(object).hasClass("selected-icon")){
+		$(object).removeClass("selected-icon");
+	} else {
+		$(object).siblings().removeClass("selected-icon");
+		$(object).addClass("selected-icon");
+	}
+}
+
+function thumbsDown(object){
+	if($(object).hasClass("selected-icon")){
+		$(object).removeClass("selected-icon");
+	} else {
+		$(object).siblings().removeClass("selected-icon");
+		$(object).addClass("selected-icon");
+	}
+}
+
+function nextArea(){
+	$(".selected-icon").removeClass("selected-icon");
+	switchGrey();
+}
+
+///Second ranked example
+
+function switchGrey2(){
+	var grey1 = $(".grey1-part2");
+	var grey2 = $(".grey2-part2");
+	var grey21 = $(".grey2-1-part2");
+	var grey3 = $(".grey3-part2");
+	var highlighted1 = $(".highlighted1-part2");
+	var highlighted2 = $(".highlighted2-part2");
+	var highlighted3 = $(".highlighted3-part2");
+
+	if($(grey1).css("display") == "block"){
+		//move to second part
+		$(grey1).fadeOut("slow");
+		$(grey2).fadeIn("slow");
+		$(grey21).fadeIn("slow");
+		$(highlighted1).fadeOut("slow");
+		$(highlighted2).fadeIn("slow");
+	} else if($(grey2).css("display") == "block"){
+		// move to third part
+		$(grey2).fadeOut("slow");
+		$(grey21).fadeOut("slow");
+		$(grey3).fadeIn("slow");
+		$(highlighted2).fadeOut("slow");
+		$(highlighted3).fadeIn("slow");
+		$("#goHome").fadeIn("slow");
+	} else {
+		// move to first part
+		$(grey3).fadeOut("slow");
+		$(grey1).fadeIn();
+		$(highlighted3).fadeOut("slow");
+		$(highlighted1).fadeIn("slow");
+		$("#goHome").fadeOut("slow");
+	}
+}
+
+function nextArea2(){
+	$(".selected-icon").removeClass("selected-icon");
+	switchGrey2();
+}
+
+
+function drawVerticalLine(event){
+	var c = document.getElementById("vertical_canvas");
+	var x = event.clientX;
+	var y = event.clientY;
+	//x -= c.offsetLeft;
+	//y -= c.offsetTop;
+
+	x = x - $("canvas").offset().left;
+
+	console.log("x is at: " + x + " and y is at: " + y);
+
+	var ctx = c.getContext("2d");
+	ctx.moveTo(x,0);
+	ctx.lineTo(x,550);
+	ctx.stroke();
+
+	current = parseInt($("#count").html());
+	$("#count").html(current+1);
+
+	linesArray.push(x);
+}
+
+function drawFeedbackLines(){
+	var c = document.getElementById("vertical_canvas");
+	console.log("hello");
+	var ctx = c.getContext("2d");
+	ctx.moveTo(190,0);
+	ctx.lineTo(190,550);
+	ctx.lineWidth = 7;
+
+	ctx.strokeStyle = "#81F79F";
+	ctx.stroke();
+
+	ctx.moveTo(18,0);
+	ctx.lineTo(18,550);
+	ctx.lineWidth = 5;
+	ctx.strokeStyle = "#81F79F";
+	ctx.stroke();
+
+	ctx.moveTo(165,0);
+	ctx.lineTo(165,550);
+	ctx.lineWidth = 4;
+	ctx.strokeStyle = "#81F79F";
+	ctx.stroke();
+}
+
+function drawPastLines(array){
+	var c = document.getElementById("vertical_canvas");
+
+	var ctx = c.getContext("2d");
+
+	for(var i=0; i < array.length; i++){
+		ctx.moveTo(array[i], 0);
+		ctx.lineTo(array[i], 550);
+		ctx.lineWidth = 2;
+		ctx.strokeStyle = "#99D6FF";
+		ctx.stroke();
+		console.log(array[i]);
+	}
+}
+
