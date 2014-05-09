61bc6b0418e27d784fa218f83812c0b6a91b437a
phouse512 <philiphouse2015@u.northwestern.edu>
2014-05-09 14:55:13 -0500

    working stuff

--- delta-project.js
+++ delta-project.js
@@ -99,7 +99,7 @@ if(Meteor.isClient){
         console.log(this.params._test);
         differentials = LineDifferentials.find({image: this.params._test}).fetch();
         reports = WrittenHeuristics.find({image: this.params._test}, {sort: {date_created: -1}}).fetch();
-        return {differentials: differentials, reports: reports};
+        return {reports: reports};
       }
     });
   });
@@ -120,15 +120,62 @@ if(Meteor.isClient){
     'mouseover #strongVertical': function() {
       var c = document.getElementById("target_form_canvas");
       var ctx = c.getContext("2d");
-      ctx.moveTo(20,0);
-      ctx.lineTo(20, 400);
+      ctx.moveTo(10,0);
+      ctx.lineTo(10, 400);
       ctx.stroke();
+
+      var d = document.getElementById("michaels_form_canvas");
+      var dtx = d.getContext("2d");
+      dtx.moveTo(350,0);
+      dtx.lineTo(350,400);
+      dtx.stroke();
+
+      dtx.moveTo(190,0);
+      dtx.lineTo(190,400);
+      dtx.stroke();
+      var description = "Both forms contain strong vertical lines - the form on the left is vertically aligned to the left and the form on the right is vertically aligned to the right.";
+      $("#description").html(description);
     }, 
     'mouseleave #strongVertical': function() {
       var c = document.getElementById("target_form_canvas");
       var context = c.getContext('2d');
       context.beginPath();
       context.clearRect(0, 0, c.width, c.height);
+
+      var d = document.getElementById("michaels_form_canvas");
+      var dontext = d.getContext('2d');
+      dontext.beginPath();
+      dontext.clearRect(0,0, d.width, d.height);
+      $("#description").html("");
+
+    },
+    'mouseover #buttonLine': function() {
+      var c = document.getElementById("target_form_canvas");
+      var ctx = c.getContext("2d");
+      ctx.moveTo(10,0);
+      ctx.lineTo(10, 400);
+      ctx.stroke();
+
+      var d = document.getElementById("michaels_form_canvas");
+      var dtx = d.getContext("2d");
+      dtx.moveTo(345,0);
+      dtx.lineTo(345,400);
+      dtx.stroke();
+
+      var description = "Both forms have buttons that help advance the forms.  In one form, the button is aligned to the left ON the strong vertical line, and in the other form, the button is aligned to the right on the strong vertical line as well.";
+      $("#description").html(description);
+    }, 
+    'mouseleave #buttonLine': function() {
+      var c = document.getElementById("target_form_canvas");
+      var context = c.getContext('2d');
+      context.beginPath();
+      context.clearRect(0, 0, c.width, c.height);
+
+      var d = document.getElementById("michaels_form_canvas");
+      var dontext = d.getContext('2d');
+      dontext.beginPath();
+      dontext.clearRect(0,0, d.width, d.height);
+      $("#description").html("");
     }
   });
 
@@ -190,6 +237,9 @@ if(Meteor.isClient){
       //differentials = strong - weak
       userDifferential = strongLines.length - weakLines.length;
       LineDifferentials.insert({image: 'test1', differential: userDifferential});
+      
+      Router.go('/written-heuristics/1');
+
     }
   });
 
@@ -201,6 +251,8 @@ if(Meteor.isClient){
       //differentials = strong - weak
       userDifferential = strongLines.length - weakLines.length;
       LineDifferentials.insert({image: 'test2', differential: userDifferential});
+    
+      Router.go('/written-heuristics/2');
     }
   });
 
@@ -224,6 +276,10 @@ if(Meteor.isClient){
           }, 1500);
         });
       });
+    },
+
+    'click #exit': function(){
+      Router.go('/view-heuristics/test1');
     }
   });
 
@@ -246,8 +302,71 @@ if(Meteor.isClient){
           }, 1500);
         });
       });
+    },
+
+    'click #exit': function(){
+      Router.go('/view-heuristics/test2');
     }
   });
+
+  Template.ViewHeuristics.priorities = function(){
+    image = Router.current().path.substring(17);
+    var list = [["Cosmetic Problems: ", 0],["Minor Problems: ", 0],["Major Problems: ", 0], ["Catastrophic Problems: ", 0]];
+    cosmetic = WrittenHeuristics.find({image: image, priority: 'cosmetic'}).fetch();
+    list[0][1] = cosmetic.length;
+    minor = WrittenHeuristics.find({image: image, priority: 'minor'}).fetch();
+    list[1][1] = minor.length;
+    major = WrittenHeuristics.find({image: image, priority: 'major'}).fetch();
+    list[2][1] = major.length;
+    catastrophic = WrittenHeuristics.find({image: image, priority: 'catastrophic'}).fetch();
+    list[3][1] = catastrophic.length;
+    console.log(list);
+
+    return list;
+  }
+
+  Template.ViewHeuristics.prioritiesList = function(){
+    list = ["Cosmetic Problems: ", "Minor Problems: ", "Major Problems: ", "Catastrophic Problems: "];
+    return list;
+  }
+
+  Template.ViewHeuristics.averageDifferentials = function(){
+    image = Router.current().path.substring(17);
+    differentials = LineDifferentials.find({image: image}).fetch();
+    console.log("Differential array: " + differentials);
+    var total = 0;
+    $.each(differentials,function() {
+        total += this.differential;
+    });
+    console.log(total);
+
+    var avg = total / differentials.length;
+    return avg;
+  }
+
+  Template.ViewHeuristics.greaterThanOrLessThan = function(){
+    image = Router.current().path.substring(17);
+    differentials = LineDifferentials.find({image: image}).fetch();
+    console.log("Differential array: " + differentials);
+    var total = 0;
+    $.each(differentials,function() {
+        total += this.differential;
+    });
+    console.log(total);
+
+    var avg = total / differentials.length;
+    
+    if (avg > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  
+  Template.ViewHeuristics.totalUsers = function(array){
+    return array.length;
+  } 
 }
 
 if (Meteor.isServer) {
